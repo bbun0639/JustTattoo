cd17fe7c4ae5ff71e36f84c98deaad8c8eb3b506
bbun0639 <pathomphob_sun@cmu.ac.th>
2021-03-22 18:03:07 +0700

    Send data img by object

--- src/App.js
+++ src/App.js
@@ -4,23 +4,17 @@ import AppHeader from './components/AppHeader';
 import TattooItem from './components/TattooItem';
 import TattooPost from './components/TattooPost';
 
+const tattoo1 =  {  //send data by object
+    title: "แขนเท่ห์ๆ",
+    thumbnailUrl: "/images/tattoo-01-small.jpg"
+}
+
 function App() {
-
-    const textInput = <input type="text" />
-    const okBtn = (
-        <button>
-            สักหน่อยไหมล่ะ
-        </button>
-    );
-
     return (
         <div className="App">
             <AppHeader />
             <div className="app-grid">
-                <TattooItem title="แขนเท่ห์ๆ" thumbnailUrl="/images/tattoo-01-small.jpg" />
-                <TattooItem title="มือเท่ห์ๆ" thumbnailUrl="/images/tattoo-02-small.jpg" />
-                <TattooItem title="คอเท่ห์ๆ" thumbnailUrl="/images/tattoo-03-small.jpg" />
-                <TattooItem title="หลังเท่ห์ๆ" thumbnailUrl="/images/tattoo-04-small.jpg" />
+                <TattooItem tattoo={tattoo1} />
             </div>
             {/* <TattooPost/> */}
         </div>
