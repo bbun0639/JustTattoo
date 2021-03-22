708d1dd0c18ade8c8f3d543282288a46edc4f510
bbun0639 <pathomphob_sun@cmu.ac.th>
2021-03-22 18:36:55 +0700

    send data img by array[] obj

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import AppHeader from './components/AppHeader';
 import TattooItem from './components/TattooItem';
 import TattooPost from './components/TattooPost';
 
-const tattoo1 =  {  //send data by object
-    title: "แขนเท่ห์ๆ",
-    thumbnailUrl: "/images/tattoo-01-small.jpg"
-}
+const tattoos = [
+    {
+        title: "แขนเท่ห์ๆ",
+        thumbnailUrl: "/images/tattoo-01-small.jpg"
+    },
+    {
+        title: "แขนเท่ห์ๆ",
+        thumbnailUrl: "/images/tattoo-02-small.jpg"
+    }
+];
 
 function App() {
+    const tattooElements = tattoos.map((tattoo, index) => {
+        return <TattooItem key={index} tattoo={tattoo} />;
+    })
     return (
         <div className="App">
             <AppHeader />
             <div className="app-grid">
-                <TattooItem tattoo={tattoo1} />
+                {tattooElements}
             </div>
             {/* <TattooPost/> */}
         </div>
